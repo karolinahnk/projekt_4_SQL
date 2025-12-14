8511b90239258b1d4c3233539034449e1cea057f
karolinahnk <karolinahynkova97@seznam.cz>
2025-12-14 17:05:54 +0100

    Update 5.question.sql

--- 5.question.sql
+++ 5.question.sql
@@ -2,45 +2,55 @@
 --výrazněji v jednom roce, projeví se to na cenách potravin či mzdách ve stejném nebo následujícím 
 --roce výraznějším růstem?
 
-with base as (
-	select 
-		p.year,
-		p.avg_wage,
-		(p.price_milk + p.price_bread) / 2 as avg_food_price,
-		e.gdp
-	from t_karolina_hynkova_project_sql_primary_final p
-	join economies e 
-		on p.year = e.year 
-		and (e.country = 'Czech Republic' or e.country ilike '%Czech%')
+WITH food_avg_per_year AS (
+    SELECT
+        year,
+        AVG(avg_price) AS avg_food_price
+    FROM t_karolina_hynkova_project_sql_primary_final
+    GROUP BY year
 ),
-yoy as (
-	select 
-		year,
-		avg_wage,
-		gdp,
-		avg_food_price,
-		round(((avg_wage - lag(avg_wage) over(order by year))
-			/ nullif(lag(avg_wage) over(order by year), 0))::numeric, 2) as wage_yoy,
-		round(((gdp - lag(gdp) over(order by year))
-			/ nullif(lag(gdp) over(order by year), 0))::numeric, 2) as gdp_yoy,
-		round(((avg_food_price -lag(avg_food_price) over(order by year))
-			/ nullif(lag(avg_food_price) over(order by year), 0))::numeric, 2) as food_yoy
-	from base
+base AS (
+    SELECT
+        f.year,
+        f.avg_food_price,
+        w.avg_wage,
+        s.gdp
+    FROM food_avg_per_year f
+    JOIN (
+        SELECT
+            year,
+            AVG(avg_wage) AS avg_wage
+        FROM t_karolina_hynkova_project_sql_primary_final
+        GROUP BY year
+    ) w USING (year)
+    JOIN t_karolina_hynkova_project_sql_secondary_final s
+        ON f.year = s.year
+       AND s.country = 'Czech Republic'
 ),
-with_leads as (
-	select
-		year,
-		gdp_yoy,
-		wage_yoy,
-		food_yoy,
-		lead(wage_yoy) over (order by year) as wage_yoy_next,
-		lead(food_yoy) over (order by year) as food_yoy_next
-	from yoy
+yoy AS (
+    SELECT
+        year,
+        ROUND(((avg_food_price - LAG(avg_food_price) OVER (ORDER BY year))
+        / NULLIF(LAG(avg_food_price) OVER (ORDER BY year), 0))::numeric, 2) AS food_yoy,
+        ROUND(((avg_wage - LAG(avg_wage) OVER (ORDER BY year))
+        / NULLIF(LAG(avg_wage) OVER (ORDER BY year), 0))::numeric, 2) AS wage_yoy,
+        ROUND(((gdp - LAG(gdp) OVER (ORDER BY year))
+        / NULLIF(LAG(gdp) OVER (ORDER BY year), 0))::numeric, 2) AS gdp_yoy
+    FROM base
+),
+final AS (
+    SELECT
+        year,
+        gdp_yoy,
+        wage_yoy,
+        food_yoy,
+        LEAD(wage_yoy) OVER (ORDER BY year) AS wage_yoy_next_year,
+        LEAD(food_yoy) OVER (ORDER BY year) AS food_yoy_next_year
+    FROM yoy
 )
-select *
-from with_leads
-order by year;
+SELECT *
+FROM final
+ORDER BY year;
 
---výpočty pro meziroční změny DGP, mzdy, potravin(průměr všech položek)
--- na základě výsledků nelze potvrdit, ale ani vyvrátit, že by tyto faktory měly přímý vliv na cenu potravin
--- u mezd se souvislost objevuje. Pokud HDP v jednom roce výrazně vzroste další rok je i růst mezd
+-- výpočet průměrné ceny všech potravin za rok
+-- následný výpočet meziročních změn 
