a48bd25b55ae108924e1b9d68de93d845b27276d
karolinahnk <karolinahynkova97@seznam.cz>
2026-01-06 16:16:12 +0100

    Update 3.question.sql

--- 3.question.sql
+++ 3.question.sql
@@ -1,37 +1,31 @@
---Která kategorie potravin zdražuje nejpomaleji (je u ní nejnižší percentuální meziroční nárůst)? 
+--- 3. otázka: Která kategorie potravin zdražuje nejpomaleji (je u ní nejnižší percentuální meziroční nárůst)? 
 
-with yearly_prices as (
-	select
-		pc.name as category,
-		extract(year from cp.date_from) as year,
-		round(avg(cp.value)::numeric, 2) as avg_price
-	from czechia_price cp 
-	join czechia_price_category pc
-		on cp.category_code = pc.code 
-	group by pc.name, extract(year from cp.date_from) --spočítání průměrné ceny pro každou kategorii v každém roce
+WITH yearly_prices AS (
+	SELECT
+        food_category_name AS category,
+        year,
+        ROUND(AVG(avg_price), 2) AS avg_price
+    FROM t_karolina_hynkova_project_sql_primary_final
+    GROUP BY food_category_name, year
 ),
-yearly_pct_change as (
-	select 
-		category,
-		year,
-		avg_price,
-		lag(avg_price) over (partition by category order by year) as prev_price,
-		round( 
-			(avg_price - lag(avg_price) over (partition by category order by year))
-			/ nullif(lag(avg_price) over (partition by category order by year), 0) * 100,
-			2
-		) as pct_change
-	from yearly_prices
+yearly_pct_change AS (
+    SELECT
+        category,
+        year,
+        avg_price,
+        LAG(avg_price) OVER (PARTITION BY category ORDER BY year) AS prev_price,
+        ROUND((avg_price - LAG(avg_price) OVER (PARTITION BY category
+        ORDER BY year))
+        / NULLIF(LAG(avg_price) OVER (PARTITION BY category ORDER BY year), 0) * 100, 2) AS pct_change
+    FROM yearly_prices
 )
---procentuální vývoj ceny potravin pro každý rok
-select 
-	category,
-	round(avg(pct_change), 2) as avg_yearly_pct_growth
-from yearly_pct_change
-where pct_change is not null
-group by category
-order by avg_yearly_pct_growth asc
-limit 1;
-
--- pomocí dvou pomocných tabulek jedna pro průměrné ceny potravin pro každý rok a druhá pro procentuální změny
--- mezi roky. Jsme zjistili, že cena cukru krystal se každý rok snižovala tempem -1,92%
+SELECT
+    category,
+    ROUND(AVG(pct_change), 2) AS avg_yearly_pct_growth
+FROM yearly_pct_change
+WHERE pct_change IS NOT NULL
+GROUP BY category
+ORDER BY avg_yearly_pct_growth ASC
+LIMIT 1;
+-- sjednocení cen za rok AVG(avg_price), zjištění procentuálního vývoje potravin pro každý rok
+-- vybrýní kategorie potravin, která zdražuje nejpomaleji
